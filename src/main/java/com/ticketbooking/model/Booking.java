0eddf480e9e96570c5ea096ef67f37bacb95135e
IT25100561 <it25100561@my.sliit.lk>
2026-05-24 20:18:27 +0530

    Version 2

--- src/main/java/com/ticketbooking/model/Booking.java
+++ src/main/java/com/ticketbooking/model/Booking.java
@@ -2,15 +2,18 @@ package com.ticketbooking.model;
 
 import com.ticketbooking.util.CsvUtil;
 
-public class Booking extends Entity {
-    private String userId;
-    private String eventId;
-    private String seats;
-    private double totalPrice;
-    private String status;
+// RELATIONSHIP &  Booking inherits from Entity class (Inheritance concept).
+public class Booking extends Entity { 
+    //  Encapsulation  Making fields private.
+    private String userId; // Stores the id of the user who made the booking.
+    private String eventId; // Stores the id of the booked event.
+    private String seats; // Stores seat numbers as a comma separated string.
+    private double totalPrice; // Stores the total cost of the booking.
+    private String status; //  confirmed, cancelled.
 
-    public Booking() {}
+    public Booking() {} // Default empty constructor required for object instantiation.
 
+    //  Encapsulation  Public getters and setters to access private fields.
     public String getUserId() { return userId; }
     public void setUserId(String v) { this.userId = v; }
     public String getEventId() { return eventId; }
@@ -22,11 +25,13 @@ public class Booking extends Entity {
     public String getStatus() { return status; }
     public void setStatus(String v) { this.status = v; }
 
-    @Override
+    @Override // Polymorphism  Overriding the serialize method from the parent Entity class.
     public String serialize() {
+        // Converts the object's properties into a CSV formatted string for file storage.
         return CsvUtil.join(id, userId, eventId, seats, totalPrice, status, createdAt);
     }
 
+    // Static method used to create a Booking object from a CSV line .
     public static Booking deserialize(String line) {
         String[] p = CsvUtil.split(line);
         Booking b = new Booking();
@@ -34,9 +39,13 @@ public class Booking extends Entity {
         b.userId = CsvUtil.unescape(p[1]);
         b.eventId = CsvUtil.unescape(p[2]);
         b.seats = CsvUtil.unescape(p[3]);
+        
+        // ERROR HANDLING Try catch block to handle potential NumberFormatException during parsing.
         try { b.totalPrice = Double.parseDouble(CsvUtil.unescape(p[4])); } catch (Exception ex) { b.totalPrice = 0; }
-        b.status = CsvUtil.unescape(p[5]);
-        b.createdAt = p.length > 6 ? CsvUtil.unescape(p[6]) : "";
+        
+        b.status = CsvUtil.unescape(p[5]); // Sets Status.
+        // Checks if createdAt exists in the array to prevent IndexOutOfBoundsException.
+        b.createdAt = p.length > 6 ? CsvUtil.unescape(p[6]) : ""; 
         return b;
     }
 }
